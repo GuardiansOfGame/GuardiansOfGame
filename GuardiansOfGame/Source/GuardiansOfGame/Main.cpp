138392c80b41fcdeae6fa71b56fbb429e96c9ec1
DESKTOP-UOQM1PH\YANG <meekk1009@gmail.com>
2022-04-17 18:22:09 +0900

    Main Character Class 생성

---
+++ GuardiansOfGame/Source/GuardiansOfGame/Main.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Main.h"
+
+// Sets default values
+AMain::AMain()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMain::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMain::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMain::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AMain::MoveForward(float Value)
+{
+}
+
+void AMain::MoveRight(float Value)
+{
+}
+
+void AMain::Turn(float Rate)
+{
+}
+
+void AMain::LookUp(float Rate)
+{
+}
+
+void AMain::Interaction()
+{
+}
+
+void AMain::SpaceDown()
+{
+}
+
+void AMain::SpaceUp()
+{
+}
+
+void AMain::LCtrlDown()
+{
+}
---
+++ GuardiansOfGame/Source/GuardiansOfGame/Main.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Main.generated.h"
+
+UCLASS()
+class GUARDIANSOFGAME_API AMain final : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMain();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+protected:
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+	void Turn(float Rate);
+	void LookUp(float Rate);
+
+	void Interaction();
+
+	void SpaceDown();
+	void SpaceUp();
+
+	void LCtrlDown();
+
+public:
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+};
