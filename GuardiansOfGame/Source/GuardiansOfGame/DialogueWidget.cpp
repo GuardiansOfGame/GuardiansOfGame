155c155b3739d5f77de6a2da66834ce17f4324a2
DESKTOP-UOQM1PH\YANG <meekk1009@gmail.com>
2022-04-18 22:53:51 +0900

    DialogueWidget Class 생성

---
+++ GuardiansOfGame/Source/GuardiansOfGame/DialogueWidget.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DialogueWidget.h"
+
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Kismet/GameplayStatics.h"
+
+#include "MainController.h"
+
+void UDialogueWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	SpeechBubble->OnClicked.AddDynamic(this, &UDialogueWidget::SpeechBubbleClicked);
+}
+
+void UDialogueWidget::SpeechBubbleClicked()
+{
+	if(++DialogueIndex < DialogueLength)
+	{
+		SetDialogue();
+	}
+	else
+	{
+		DialogueIndex = 0;
+		SetDialogue();
+
+		End();
+	}
+}
+
+void UDialogueWidget::Begin(const TArray<FString> Strings, const FString IName)
+{
+	DialogueStrings = Strings;
+	DialogueLength = DialogueStrings.Num();
+	DialogueIndex = 0;
+
+	const FText Name = FText::FromString(IName);
+	SpeakerName->SetText(Name);
+
+	SetDialogue();
+}
+
+void UDialogueWidget::End()
+{
+	AMainController* Controller = Cast<AMainController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+	if(Controller)
+	{
+		
+	}
+}
+
+void UDialogueWidget::SetDialogue()
+{
+	const FText CurrentText = FText::FromString(DialogueStrings[DialogueIndex]);
+	DialogueText->SetText(CurrentText);
+}
---
+++ GuardiansOfGame/Source/GuardiansOfGame/DialogueWidget.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DialogueWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUARDIANSOFGAME_API UDialogueWidget final : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UCanvasPanel* CanvasPanel;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* SpeechBubble;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* SpeakerName;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* DialogueText;
+
+	UPROPERTY(VisibleAnywhere, Category = Dialogue)
+	TArray<FString> DialogueStrings;
+
+	UPROPERTY(VisibleAnywhere, Category = Dialogue)
+	int DialogueLength;
+
+	UPROPERTY(VisibleAnywhere, Category = Dialogue)
+	int DialogueIndex;
+
+protected:
+	UFUNCTION()
+	void SpeechBubbleClicked();
+
+public:
+	void Begin(const TArray<FString> Strings, const FString IName);
+	void End();
+
+	void SetDialogue();
+};
