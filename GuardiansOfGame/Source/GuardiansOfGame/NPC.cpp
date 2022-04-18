e9951dacd5115daa50e75244bfc1a82b35f8e0b3
DESKTOP-UOQM1PH\YANG <meekk1009@gmail.com>
2022-04-18 16:18:50 +0900

    NPC Class 생성

---
+++ GuardiansOfGame/Source/GuardiansOfGame/NPC.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NPC.h"
+
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ANPC::ANPC()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	SetRootComponent(Mesh);
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> NPCMesh(TEXT("SkeletalMesh'/Game/Character/ybot/ybot.ybot'"));
+	if (NPCMesh.Succeeded())
+	{
+		Mesh->SetSkeletalMesh(NPCMesh.Object);
+		Mesh->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
+
+		if (Mesh->GetMaterial(0))
+		{
+			static ConstructorHelpers::FObjectFinder<UMaterialInterface> PurpleMaterialAsset(TEXT("Material'/Game/Character/ybot/Alpha_Body_MAT1.Alpha_Body_MAT1'"));
+			if (PurpleMaterialAsset.Succeeded())
+			{
+				UMaterialInterface* PurpleMaterial = PurpleMaterialAsset.Object;
+				Mesh->SetMaterial(0, PurpleMaterial);
+			}
+		}
+	}
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	BoxCollision->SetupAttachment(GetRootComponent());
+	BoxCollision->SetBoxExtent(FVector(128.0f, 100.0f, 32.0f));
+	BoxCollision->SetRelativeLocation(FVector(0.0f, 82.0f, 33.0f));
+}
+
+// Called when the game starts or when spawned
+void ANPC::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ANPC::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+						  int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void ANPC::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+						int32 OtherBodyIndex)
+{
+}
---
+++ GuardiansOfGame/Source/GuardiansOfGame/NPC.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NPC.generated.h"
+
+UCLASS()
+class GUARDIANSOFGAME_API ANPC : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ANPC();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = Mesh)
+	USkeletalMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, Category = Collision)
+	class UBoxComponent* BoxCollision;
+
+protected:
+	UFUNCTION()
+	virtual void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	virtual void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:
+	
+};
