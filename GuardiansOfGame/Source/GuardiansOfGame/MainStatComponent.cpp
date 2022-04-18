dec3ee10986f2d0d46ff4eeada8dcd11032b04d1
DESKTOP-UOQM1PH\YANG <meekk1009@gmail.com>
2022-04-19 01:36:54 +0900

    MainStatComponent Class 생성

---
+++ GuardiansOfGame/Source/GuardiansOfGame/MainStatComponent.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MainStatComponent.h"
+
+#include "GOGGameInstance.h"
+
+// Sets default values for this component's properties
+UMainStatComponent::UMainStatComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+
+// Called when the game starts
+void UMainStatComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UMainStatComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	Init();
+}
+
+void UMainStatComponent::Init()
+{
+	GameInstance = Cast<UGOGGameInstance>(GetWorld()->GetGameInstance());
+	if(GameInstance)
+	{
+		QuestProgress = GameInstance->GetQuestProgress();
+		CurQuestNum = GameInstance->GetCurQuestNum();
+	}
+}
+
+void UMainStatComponent::Save() const
+{
+	if (GameInstance)
+	{
+		GameInstance->SetQuestProgress(QuestProgress);
+		GameInstance->SetCurQuestNum(CurQuestNum);
+	}
+}
---
+++ GuardiansOfGame/Source/GuardiansOfGame/MainStatComponent.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+
+#include "CustomStruct.h"
+
+#include "MainStatComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class GUARDIANSOFGAME_API UMainStatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UMainStatComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	virtual void InitializeComponent() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = GameInstance)
+	class UGOGGameInstance* GameInstance;
+
+	UPROPERTY(VisibleAnywhere, Category = Quest)
+	FQuestBooleans QuestProgress;
+
+	UPROPERTY(VisibleAnywhere, Category = Quest)
+	int CurQuestNum;
+
+public:
+	FORCEINLINE FQuestBooleans GetQuestProgress() const { return QuestProgress; }
+
+	FORCEINLINE int GetCurQuestNum() const { return CurQuestNum; }
+	FORCEINLINE void SetCurQuestNum(const int Num) { CurQuestNum = Num; }
+
+	FORCEINLINE void SetCurQuestAccept(const bool bValue) { QuestProgress.CurQuestAccept = bValue; }
+	FORCEINLINE void SetCurQuestSuccess(const bool bValue) { QuestProgress.CurQuestSuccess = bValue; }
+
+	FORCEINLINE void SetQuestAcceptArr(const int QuestNum, const bool bValue) { QuestProgress.QuestAcceptArr[QuestNum] = bValue; }
+	FORCEINLINE void SetQuestSuccessArr(const int QuestNum, const bool bValue) { QuestProgress.QuestSuccessArr[QuestNum] = bValue; }
+
+	void Init();
+	void Save() const;
+};
