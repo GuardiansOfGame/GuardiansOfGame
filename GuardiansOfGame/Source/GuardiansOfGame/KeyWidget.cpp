1c84be3cc8a28d9623aa334b54c90e7b9e83b6da
DESKTOP-UOQM1PH\YANG <meekk1009@gmail.com>
2022-04-18 17:19:55 +0900

    KeyWidget Class 생성

---
+++ GuardiansOfGame/Source/GuardiansOfGame/KeyWidget.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "KeyWidget.h"
+
+#include "Components/Image.h"
+
+UKeyWidget::UKeyWidget(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	FrameImage = LoadObject<UTexture2D>(nullptr, TEXT("Texture2D'/Game/Image/KeyFrameImage.KeyFrameImage'"));
+	KeyImage = LoadObject<UTexture2D>(nullptr, TEXT("Texture2D'/Game/Image/FKeyInsideImage.FKeyInsideImage'"));
+}
+
+void UKeyWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	Frame->SetBrushFromTexture(FrameImage);
+	Key->SetBrushFromTexture(KeyImage);
+}
---
+++ GuardiansOfGame/Source/GuardiansOfGame/KeyWidget.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "KeyWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUARDIANSOFGAME_API UKeyWidget final : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	explicit UKeyWidget(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UCanvasPanel* CanvasPanel;
+
+	UPROPERTY(meta = (BindWidget))
+	class UImage* Frame;
+	UPROPERTY(meta = (BindWidget))
+	UImage* Key;
+
+	UPROPERTY(VisibleAnywhere, Category = ImageAsset)
+	UTexture2D* FrameImage;
+	UPROPERTY(VisibleAnywhere, Category = ImageAsset)
+	UTexture2D* KeyImage;
+	
+	UPROPERTY(meta = (BindWidgetAnim), Transient)
+	UWidgetAnimation* PopUpAnimation;
+
+public:
+	FORCEINLINE UWidgetAnimation* GetPopUpAnimaition() const { return PopUpAnimation; }
+};
