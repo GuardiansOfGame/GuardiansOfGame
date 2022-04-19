fde3754dec65be2b5b71414e2823aef93685e6fb
DESKTOP-UOQM1PH\YANG <meekk1009@gmail.com>
2022-04-19 21:22:23 +0900

    GetGameInstance Correction

--- GuardiansOfGame/Source/GuardiansOfGame/MainStatComponent.cpp
+++ GuardiansOfGame/Source/GuardiansOfGame/MainStatComponent.cpp
@@ -29,7 +29,7 @@ void UMainStatComponent::InitializeComponent()
 
 void UMainStatComponent::Init()
 {
-	GameInstance = Cast<UGOGGameInstance>(GEngine->GetWorld()->GetGameInstance());
+	GameInstance = Cast<UGOGGameInstance>(GetWorld()->GetGameInstance());
 	if(GameInstance)
 	{
 		QuestProgress = GameInstance->GetQuestProgress();
