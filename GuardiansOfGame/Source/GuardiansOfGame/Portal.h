9b8114c10cd649caa9747e171a6ab9d638761643
DESKTOP-UOQM1PH\YANG <meekk1009@gmail.com>
2022-04-29 23:45:47 +0900

    cleanup

--- GuardiansOfGame/Source/GuardiansOfGame/Portal.h
+++ GuardiansOfGame/Source/GuardiansOfGame/Portal.h
@@ -31,5 +31,5 @@ private:
 
 public:
 	UFUNCTION()
-	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
